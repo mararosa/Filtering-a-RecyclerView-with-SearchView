4e38f44dec213ab678f39d3fa75f11bd41c49e91
Mara Rosa <edimara.rosa@outlook.com>
2020-12-20 22:47:30 -0300

    Adding the Recyclerview in Activity

--- app/src/main/java/com/codingwithmitch/filteringrecyclerview/view/MainActivity.java
+++ app/src/main/java/com/codingwithmitch/filteringrecyclerview/view/MainActivity.java
@@ -1,16 +1,67 @@
 package com.codingwithmitch.filteringrecyclerview.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
+import com.codingwithmitch.filteringrecyclerview.Name;
 import com.codingwithmitch.filteringrecyclerview.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<Name> nameList = new ArrayList<>();
+    private SearchView searchView;
+    private RecyclerView recyclerView;
+    private AnimalsAdapter animalsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        searchView = findViewById(R.id.search_view);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        addNameToList("Giraffe");
+        addNameToList("Tiger");
+        addNameToList("Rhinoceros");
+        addNameToList("Cat");
+        addNameToList("Dog");
+        addNameToList("Bird");
+        addNameToList("Lion");
+        addNameToList("Elephant");
+        addNameToList("Bear");
+        addNameToList("Cattle");
+        addNameToList("Wolf");
+        addNameToList("Rabbit");
+        addNameToList("Snakes");
+        addNameToList("Whales");
+        addNameToList("Monkey");
+        addNameToList("Cow");
+        addNameToList("Fish");
+        addNameToList("Coyote");
+        
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        animalsAdapter = new AnimalsAdapter(this, nameList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(animalsAdapter);
+    }
+
+    private void addNameToList(String name) {
+        nameList.add(new Name(name));
     }
 }
